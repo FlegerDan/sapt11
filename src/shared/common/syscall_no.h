051116f5e8e9c7a420a1f01e4fb585b451da1117
Fleger <dan_fleger@yahoo.com>
2020-12-14 21:02:21 +0200

    yes

---
+++ src/shared/common/syscall_no.h
@@ -0,0 +1,34 @@
+#pragma once
+
+typedef enum _SYSCALL_ID
+{
+    SyscallIdIdentifyVersion,
+
+    // Thread Management
+    SyscallIdThreadExit,
+    SyscallIdThreadCreate,
+    SyscallIdThreadGetTid,
+    SyscallIdThreadWaitForTermination,
+    SyscallIdThreadCloseHandle,
+
+    // Process Management
+    SyscallIdProcessExit,
+    SyscallIdProcessCreate,
+    SyscallIdProcessGetPid,
+    SyscallIdProcessWaitForTermination,
+    SyscallIdProcessCloseHandle,
+
+    // Memory management 
+    SyscallIdVirtualAlloc,
+    SyscallIdVirtualFree,
+
+    // File management
+    SyscallIdFileCreate,
+    SyscallIdFileClose,
+    SyscallIdFileRead,
+    SyscallIdFileWrite,
+    //lab7
+    SyscallIdProcessGetNumberOfPages,
+    SyscallIdReadMemory,
+    SyscallIdReserved = SyscallIdFileWrite + 1
+} SYSCALL_ID;
